5bca6b4fca12c01e40d68bd41f0f2cafd6f66a31
Aji Lantang Mardika & Juang Wiantoro <pair+ajilantang.broerjuang@hacktiv8.com>
2016-09-27 15:49:34 +0700

    Submit Linear Search yoyoyo

--- linear_search.js
+++ linear_search.js
@@ -1,21 +1,43 @@
-'use strict'
+ 'use strict'
 
 // Release 0
+
 console.log("Linear Search")
 let linearSearch = (target, values) => {
-  //write your code here
+  for (let i = 0; i < random_numbers.length; i+= 1) {
+    if (target === random_numbers[i]) {
+      return i
+    }
+  }
+  return -1
 }
-
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
+
 
 console.log(linearSearch(18, random_numbers));
 // 2
+console.log(linearSearch(19,random_numbers));
 console.log(linearSearch(9, random_numbers));
 // -1
 
 // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
+  let i = 0;
+  let tempArr=[];
+  if (banana_arr.includes(target)) {
+    while (i < banana_arr.length) {
+      if (target === banana_arr[i]) {
+        tempArr.push(i);
+      }
+      i += 1;
+    }
+    return tempArr
+  }else {
+    return -1
+  }
+
+  // return -1
   //write your code here
 }
 
@@ -23,3 +45,4 @@ let banana_arr = "banana".split("");
 // ["b", "a", "n", "a", "n", "a"]
 console.log(globalLinearSearch("a", banana_arr));
 // [ 1, 3, 5 ]
+console.log(globalLinearSearch("z", banana_arr));
