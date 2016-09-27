02bf073cf5c61ac4044146e704c07105ded5c1cb
Alexander Hendrawan tjahja & Toni <pair+alexanderHT.chrisantoni@devbootcamp.com>
2016-09-27 18:59:44 +0700

    linear search

--- linear_search.js
+++ linear_search.js
@@ -2,21 +2,38 @@
 
 // Release 0
 console.log("Linear Search")
+
 let linearSearch = (target, values) => {
-  //write your code here
+  for(let i=0;i<values.length;i++)
+  {
+    if(target == values[i]){
+      return i
+    }else if( i == values.length - 1){
+      return -1
+    }
+  }
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
 
-console.log(linearSearch(18, random_numbers));
+
+console.log(" index " + linearSearch(18, random_numbers));
 // 2
-console.log(linearSearch(9, random_numbers));
+console.log(" index " + linearSearch(9, random_numbers));
 // -1
+
 
 // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
-  //write your code here
+  var tampung = []
+  for(let i=0;i<values.length;i++)
+  {
+    if(target == values[i]){
+      tampung.push(i)
+    }
+  }
+  return tampung
 }
 
 let banana_arr = "banana".split("");
