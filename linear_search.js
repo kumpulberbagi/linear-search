9718e7a248f1b5babaa89001e2454b6114370310
Elkana Lumbantobing & Septian A Tama <pair+elkana-ltobing.tamatamvan@devbootcamp.com>
2016-09-27 19:25:40 +0700

    function linear search release 0 & 1

--- linear_search.js
+++ linear_search.js
@@ -4,6 +4,18 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  let status = 0;
+  for (let i = 0; i < values.length; i++) {
+    if (target === values[i]) {
+      status += 1;
+    }
+  }
+
+  if (status === 0) {
+    status -= 1;
+  }
+
+  return status;
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -15,8 +27,21 @@ console.log(linearSearch(9, random_numbers));
 
 // Release 1
 console.log("Global Linear Search")
+
 let globalLinearSearch = (target, values) => {
   //write your code here
+  let index = [];
+
+  for (let i = 0; i < values.length; i++) {
+    if (target === values[i]) {
+      index.push(i);
+    }
+  }
+
+  if (index.length === 0) {
+    console.log("Target not found!");
+  }
+  return index;
 }
 
 let banana_arr = "banana".split("");
