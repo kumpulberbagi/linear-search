dc4c1733897f0262ec6f774bc763f1c401b6a651
Alessandro C & Mangku Widodo <pair+alessandrocls.mangkuwi@hacktiv8.com>
2016-09-27 19:11:04 +0700

    Submitted linear_search.js

--- linear_search.js
+++ linear_search.js
@@ -4,19 +4,34 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  for(var i = 0; i < values.length;i++ ){
+    if(values[i] === target ){
+      return i;
+    }
+  }
+  return -1;
 }
+
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
 
 console.log(linearSearch(18, random_numbers));
 // 2
-console.log(linearSearch(9, random_numbers));
+console.log(linearSearch(10000, random_numbers));
 // -1
 
 // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
-  //write your code here
+  var newArray = []
+  for(var i = 0; i < values.length; i++){
+    if(values[i] === target){
+      newArray.push(i)
+    }
+  }
+return newArray
+
+
 }
 
 let banana_arr = "banana".split("");
