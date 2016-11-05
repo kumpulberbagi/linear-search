45bf701c4061c1428e0a65a1d3a27e53a23dd4bc
Your Name <name@example.com>
2016-11-05 15:23:29 +0700

    Complate All

--- linear_search.js
+++ linear_search.js
@@ -2,10 +2,9 @@
 
 // Release 0
 console.log("Linear Search")
-
 let linearSearch = (target, values) => {
     for (var i = 0; i < values.length; i++) {
-        if (target== values[i]) {
+        if (target == values[i]) {
             return i
         }
     }
@@ -18,20 +17,19 @@ console.log(linearSearch(9, random_numbers));
 // -1
 
 
-
-
-
-
-
-
-
 // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
-    //write your code here
+    var hasil = []
+    for (var i = 0; i < values.length; i++) {
+      if(values[i]==target){
+        hasil.push(i)
+      }
+    }
+    return hasil;
 }
 
 let banana_arr = "banana".split("");
 // ["b", "a", "n", "a", "n", "a"]
-//console.log(globalLinearSearch("a", banana_arr));
+console.log(globalLinearSearch("a", banana_arr));
 // [ 1, 3, 5 ]
